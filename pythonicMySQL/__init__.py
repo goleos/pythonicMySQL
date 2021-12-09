42bfe069ea43e585ec5a16d22e4c5653303c7a8d
LeonidGoldberg <leonid.goldberg@outlook.com>
2021-12-09 12:48:03 +0000

    prepare to merge dataclass branch

--- pythonicMySQL/__init__.py
+++
@@ -1,5 +0,0 @@
-from pythonicMySQL.column import Column
-from pythonicMySQL.datatypes import MySQLType
-from pythonicMySQL.mysqlobject import MySQLObject
-from pythonicMySQL.table import Table
-from pythonicMySQL.query import Query
